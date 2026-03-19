75012ffaabfa4218df36c3dfa4ee0b666e38ec79
Sam Canales <scanales@northpeakcare.com>
2026-03-19 12:28:56 -0700

    Initial Commit

---
+++ MedMiles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedMiles
+//
+//  Created by Samuel Canales on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedMiles/MedMilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedMilesApp.swift
+//  MedMiles
+//
+//  Created by Samuel Canales on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MedMilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
